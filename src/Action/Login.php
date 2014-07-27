e2080ceb973dda97a842343e909883b172d3120d
Ross Riley <riley.ross@gmail.com>
2014-07-27 11:14:02 +0100

    add messages to give ui feedback

---
+++ src/Action/Login.php
@@ -0,0 +1,39 @@
+<?php
+namespace Bolt\Extensions\Action;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Bolt\Extensions\Entity;
+
+
+class Login extends AbstractAction
+{
+    
+    public function __invoke(Request $request)
+    {
+        $form = $this->forms->createBuilder()
+            ->add("email","text")
+            ->add("password", "password")
+            ->add('login', 'submit')
+            ->getForm();
+
+        $form->handleRequest();
+
+        if($form->isValid()) {
+            $repo = $this->em->getRepository(Entity\Account::class);
+            $user = $repo->findOneBy(["email"=>$form->getData()["email"]]);
+            if (null !== $user && password_verify($form->getData()["password"], $user->password)) {
+                $request->getSession()->set("bolt.user.id", $user->id);
+                $dest = ($ret = $request->getSession()->get('bolt.auth.return')) ? $ret : $this->getUrl("home");
+                $request->getSession()->remove('bolt.auth.return');
+                return new RedirectResponse($dest);
+            } else {
+                $request->getSession()->getFlashBag()->add('error', 'Login Unsuccessful!');
+            }
+        }
+        
+        return new Response( $this->renderer->render("login.html", ['form'=>$form->createView()]) );
+
+    }
+}
