bd140de54736c9321543e5334ea3d92a3c7cd0f0
Ross Riley <riley.ross@gmail.com>
2014-08-09 07:40:00 +0100

    work on package detail view

---
+++ src/Action/ViewPackage.php
@@ -0,0 +1,22 @@
+<?php
+namespace Bolt\Extensions\Action;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+use Bolt\Extensions\Entity;
+
+
+class ViewPackage extends AbstractAction
+{
+    
+    public function __invoke(Request $request, $params)
+    {
+        $repo = $this->em->getRepository(Entity\Package::class);
+        $package = $repo->findOneBy(['id'=>$params['package']]);
+        print_r($package->keywords); exit;
+        return new Response($this->renderer->render("view.html", ['package'=>$package]));
+
+    }
+}
