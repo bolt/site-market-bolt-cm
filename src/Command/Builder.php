56504a346809957a713f59e91277a992db2733c0
Ross Riley <riley.ross@gmail.com>
2014-09-09 10:56:16 +0100

    debug

--- src/Command/Builder.php
+++ src/Command/Builder.php
@@ -49,10 +49,11 @@ class Builder extends Command {
                 $input = new ArrayInput([]);
                 $returnCode = $command->run($input, $output);
                 if($returnCode === 0) {
+                    $output->writeln(print_r($_SERVER,1));
                     $output->writeln("<info>Satis file built...</info>");
                     $output->writeln(shell_exec("composer config -g github-oauth.github.com `head -1 config/github`"));
                     $output->writeln(shell_exec("chmod -R 0777 /tmp"));
-                    $output->writeln(shell_exec("vendor/bin/satis build --skip-errors -n --no-html-output"));
+                    $output->writeln(shell_exec("vendor/bin/satis build --skip-errors -n"));
                 }
                 
             } catch (\Exception $e) {
