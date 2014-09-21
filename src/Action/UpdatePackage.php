59c9c054c04fd8ea87571f0c79b3cbe4155ef6b4
Ross Riley <riley.ross@gmail.com>
2014-09-21 08:20:21 +0100

    add file and line to error

--- src/Action/UpdatePackage.php
+++ src/Action/UpdatePackage.php
@@ -24,7 +24,7 @@ class UpdatePackage extends AbstractAction
             }
         } catch (\Exception $e) {
             $request->getSession()->getFlashBag()->add('alert', "Package has an invalid composer.json and will be disabled!");
-            $request->getSession()->getFlashBag()->add('warning', $e->getMessage());
+            $request->getSession()->getFlashBag()->add('warning', implode(" : ", $e->getMessage(),$e->getFile(),$e->getLine()));
             $package->approved = false; 
         }
         
