0dbb700329e85bdcc74a7ccd892a679348b379d2
Ross Riley <riley.ross@gmail.com>
2014-10-15 17:27:47 +0100

    add star functionality

--- src/Action/Stat.php
+++ src/Action/Stat.php
@@ -7,23 +7,26 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Doctrine\ORM\EntityManager;
 use Bolt\Extensions\Entity;
+use Aura\Router\Router;
 
 
 class Stat
 {
 
-    public $em;    
+    public $em;
+    public $router;   
 
-    public function __construct(EntityManager $em)
+    public function __construct(EntityManager $em, Router $router)
     {
         $this->em = $em;
+        $this->router = $router;
     }
     
     public function __invoke(Request $request, $params)
     {
         $type = $params['id'];
         $package = $params['package'];
-        $version = $params['version'];
+        $version = isset($params['version']) ? $params['version'] :false;
         $repo = $this->em->getRepository(Entity\Package::class);
         
         $package = $repo->findOneBy(['name'=>$package]);
@@ -40,7 +43,9 @@ class Stat
         $this->em->persist($stat);
         $this->em->flush();
         
-    
+        if ($type == 'star') {
+            return new RedirectResponse($this->router->generate('view',['package'=>$package->id])); 
+        }
         $response = new JsonResponse(['status'=>'OK','package'=>$package->id]);
         $response->setCallback($request->get('callback'));
         return $response;
