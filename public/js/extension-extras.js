f287c1c2d45cf04a0b4e733bcffcab250362d3f1
Ross Riley <riley.ross@gmail.com>
2015-08-20 14:17:52 +0100

    add accordion init

--- public/js/extension-extras.js
+++ public/js/extension-extras.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($) {
     $('.popup').popup( {position    : 'bottom left'});
     $('.ui.toggle.button').popup();
+    $('.ui.accordion').accordion();
     
     var client = new ZeroClipboard( document.getElementById("copy-button") );
     client.on( "ready", function( readyEvent ) {
