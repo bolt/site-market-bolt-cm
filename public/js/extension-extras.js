d06907e039fc1e55fa30a383a5b2553e8e0660f1
Ross Riley <riley.ross@gmail.com>
2015-08-22 08:23:41 +0100

    tidy up markup and add testing block

--- public/js/extension-extras.js
+++ public/js/extension-extras.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($) {
     $('.popup').popup( {position    : 'bottom left'});
     $('.ui.toggle.button').popup();
     $('.ui.accordion').accordion();
+    $('.scrollbar-inner').scrollbar();
     
     var client = new ZeroClipboard( document.getElementById("copy-button") );
     client.on( "ready", function( readyEvent ) {
