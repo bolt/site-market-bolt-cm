cd1d4240f5e050c0737ddfe4243438ce85d0dc47
Ross Riley <riley.ross@gmail.com>
2015-10-19 22:42:28 +0100

    add migration to add new db fields

---
+++ src/Migrations/Version20151019091848.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bolt\Extensions;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151019091848 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Account ADD token VARCHAR(255) DEFAULT NULL, ADD tokenvalid DATETIME DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Account DROP token, DROP tokenvalid');
+    }
+}
