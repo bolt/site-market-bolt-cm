9d39d7298b1cccf6360290a2e14111b1343e24e2
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2016-01-23 13:50:41 +0100

    move required bolt version to changelog section

--- src/Action/ViewPackage.php
+++ src/Action/ViewPackage.php
@@ -17,13 +17,13 @@ use Bolt\Extensions\Service\PackageManager;
 
 class ViewPackage
 {
-    
+
     public $renderer;
     public $em;
     public $packageManager;
     public $router;
     public $themeservice;
-    
+
     public function __construct(Twig_Environment $renderer, EntityManager $em, PackageManager $packageManager, Router $router, BoltThemes $themeservice)
     {
         $this->renderer = $renderer;
@@ -32,19 +32,19 @@ class ViewPackage
         $this->router = $router;
         $this->themeservice = $themeservice;
     }
-    
+
     public function __invoke(Request $request, $params)
     {
         $repo = $this->em->getRepository(Entity\Package::class);
         $package = $repo->findOneBy(['id'=>$params['package']]);
-        
-        
+
+
         if(!$package) {
             $request->getSession()->getFlashBag()->add('error', "There was a problem accessing this package");
             return new RedirectResponse($this->router->generate('profile'));
         }
-        
-        $versions = ['dev'=>[],'stable'=>[]];               
+
+        $versions = ['dev'=>[],'stable'=>[]];
         $allowedit = $package->account === $request->get('user');
         $readme = $this->packageManager->getReadme($package);
 
@@ -55,20 +55,20 @@ class ViewPackage
                 $build = $repo->findOneBy(['package'=>$package->id, 'version'=>$ver['version']]);
                 if($build) {
                     $ver['build'] = $build;
-                } 
+                }
                 $versions[$ver['stability']][] = $ver;
             }
         } catch (\Exception $e) {
             $request->getSession()->getFlashBag()->add('error', $e->getMessage());
         }
-                       
+
         return new Response(
             $this->renderer->render(
-                "view.html", 
+                "view.html",
                 [
-                    'package' => $package, 
+                    'package' => $package,
                     'versions' => $versions,
-                    'readme' => $readme, 
+                    'readme' => $readme,
                     'allowedit' => $allowedit,
                     'boltthemes' => $this->themeservice->info($package)
                 ]
