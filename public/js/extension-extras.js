63e9999346f849b26b6f2d9858f7a15374c077bd
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2016-02-23 18:10:53 +0100

    load releases over ajax

--- public/js/extension-extras.js
+++ public/js/extension-extras.js
@@ -3,9 +3,17 @@ jQuery(document).ready(function($) {
     $('.ui.toggle.button').popup();
     $('.ui.accordion').accordion();
     $('.scrollbar-inner').scrollbar();
-    
+
     var client = new ZeroClipboard( document.getElementById("copy-button") );
     client.on( "ready", function( readyEvent ) {
-        
+
     });
+
+    $.ajax({
+	  	url: "/view/" + packageID + "/releases",
+	  	context: $('.releases-wrapper')
+	}).done(function(response) {
+		console.log(response);
+	  	$(this).html(response);
+	});
 });
