eac7e3aac44340a5df9dbc23c9babdf7a1322a66
Gawain Lynch <gawain.lynch@gmail.com>
2016-05-18 21:57:26 +0200

    Update Search action

--- extensions/local/bolt/site/src/Action/Search.php
+++ extensions/local/bolt/site/src/Action/Search.php
@@ -1,33 +1,39 @@
 <?php
+
 namespace Bolt\Extension\Bolt\MarketPlace\Action;
 
 use Bolt\Extension\Bolt\MarketPlace\Entity;
-use Doctrine\ORM\EntityManager;
+use Bolt\Extension\Bolt\MarketPlace\Repository\Package;
+use Bolt\Storage\EntityManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Twig_Environment;
 
-class Search
+class Search extends AbstractAction
 {
-    public $em;
-    public $renderer;
-    
-    public function __construct(Twig_Environment $renderer, EntityManager $em)
-    {
-        $this->em = $em;
-        $this->renderer = $renderer;
-    }
-    
-    public function __invoke(Request $request, $params)
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(Request $request, array $params)
     {
         $search = $request->get('q');
         $type = $request->get('type') ?: null;
         $order = $request->get('order') ?: null;
-        $repo = $this->em->getRepository(Entity\Package::class);
-        $packages = $repo->search($search, $type, $order);
-        
-        $layout = $params['type'] == 'browse' ? 'layout.twig' : 'ajax.twig';
 
-        return new Response($this->renderer->render('search.twig', ['results' => $packages, 'term' => $search, 'layout' => $layout]));
+        /** @var EntityManager $em */
+        $em = $this->getAppService('storage');
+        /** @var Package $repo */
+        $repo = $em->getRepository(Entity\Package::class);
+        $packages = $repo->search($search, $type, $order);
+
+        /** @var \Twig_Environment $twig */
+        $twig = $this->getAppService('twig');
+        $context = [
+            'results' => $packages,
+            'term'    => $search,
+            'layout'  => $params['type'] === 'browse' ? 'layout.twig' : 'ajax.twig'
+        ];
+        $html = $twig->render('search.twig', $context);
+
+        return new Response($html);
     }
 }
