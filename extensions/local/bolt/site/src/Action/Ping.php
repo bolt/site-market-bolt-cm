2f4659ec06b5fe964a4a58789870d239498c4f4a
Gawain Lynch <gawain.lynch@gmail.com>
2016-05-18 21:43:59 +0200

    Update Ping action

--- extensions/local/bolt/site/src/Action/Ping.php
+++ extensions/local/bolt/site/src/Action/Ping.php
@@ -1,23 +1,16 @@
 <?php
+
 namespace Bolt\Extension\Bolt\MarketPlace\Action;
 
-use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Twig_Environment;
 
-class Ping
+class Ping extends AbstractAction
 {
-    public $renderer;
-    public $em;
-
-    public function __construct(Twig_Environment $renderer, EntityManager $em)
-    {
-        $this->renderer = $renderer;
-        $this->em = $em;
-    }
-
-    public function __invoke(Request $request)
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(Request $request, array $params)
     {
         return new Response('pong', Response::HTTP_OK);
     }
