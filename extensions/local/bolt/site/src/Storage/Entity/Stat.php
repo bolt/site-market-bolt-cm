b1d51a32388f20b0880697631d0fdec19cd5a479
Gawain Lynch <gawain.lynch@gmail.com>
2016-05-19 16:27:53 +0200

    Import and update Stat entity

---
+++ extensions/local/bolt/site/src/Storage/Entity/Stat.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Bolt\Extension\Bolt\MarketPlace\Storage\Entity;
+
+use Doctrine\Entity\Base as EntityBase;
+
+class Stat extends EntityBase
+{
+    /** @var string */
+    protected $id;
+    /** @var string */
+    protected $package_id;
+    /** @var string */
+    protected $account_id;
+    /** @var string */
+    protected $type;
+    /** @var \DateTime */
+    protected $recorded;
+    /** @var string */
+    protected $source;
+    /** @var string */
+    protected $ip;
+    /** @var string */
+    protected $version;
+
+    protected $account;
+    protected $package;
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPackageId()
+    {
+        return $this->package_id;
+    }
+
+    /**
+     * @param string $package_id
+     */
+    public function setPackageId($package_id)
+    {
+        $this->package_id = $package_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccountId()
+    {
+        return $this->account_id;
+    }
+
+    /**
+     * @param string $account_id
+     */
+    public function setAccountId($account_id)
+    {
+        $this->account_id = $account_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getRecorded()
+    {
+        return $this->recorded;
+    }
+
+    /**
+     * @param \DateTime $recorded
+     */
+    public function setRecorded($recorded)
+    {
+        $this->recorded = $recorded;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    /**
+     * @param string $source
+     */
+    public function setSource($source)
+    {
+        $this->source = $source;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * @param string $ip
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * @param string $version
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+    }
+}
