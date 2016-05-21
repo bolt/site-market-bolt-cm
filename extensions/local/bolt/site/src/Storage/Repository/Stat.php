4e6d8bfcb0c744f2bae27e9b3c3dd67e89d7bce1
Gawain Lynch <gawain.lynch@gmail.com>
2016-05-21 13:36:50 +0200

    Repositories for Stat and VersionBuild

---
+++ extensions/local/bolt/site/src/Storage/Repository/Stat.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bolt\Extension\Bolt\MarketPlace\Storage\Repository;
+
+use Bolt\Extension\Bolt\MarketPlace\Storage\Entity;
+use Bolt\Storage\Repository;
+
+/**
+ * Stat repository.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class Stat extends AbstractRepository
+{
+}
