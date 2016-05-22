d51d2808bb9082eec5d991746c7ce2d7ed6fb361
Gawain Lynch <gawain.lynch@gmail.com>
2016-05-22 10:18:32 +0200

    Handle migration on stats and packages for duplicate addresses

--- extensions/local/bolt/market-place-migration/src/Command/AccountMigrate.php
+++ extensions/local/bolt/market-place-migration/src/Command/AccountMigrate.php
@@ -2,6 +2,8 @@
 
 namespace Bolt\Extension\Bolt\MarketPlaceMigration\Command;
 
+use Bolt\Extension\Bolt\MarketPlace\Storage\Entity as MarketPlaceEntity;
+use Bolt\Extension\Bolt\MarketPlace\Storage\Repository as MarketPlaceRepository;
 use Bolt\Extension\Bolt\MarketPlaceMigration\Storage\Entity;
 use Bolt\Extension\Bolt\MarketPlaceMigration\Storage\Repository;
 use Bolt\Extension\Bolt\Members\Storage\Entity\Account as MembersAccountEntity;
@@ -11,6 +13,7 @@ use Bolt\Extension\Bolt\Members\Storage\Repository\Account as MembersAccountRepo
 use Bolt\Extension\Bolt\Members\Storage\Repository\Oauth as MembersOauthRepository;
 use Bolt\Nut\BaseCommand;
 use Bolt\Storage\QuerySet;
+use Bolt\Storage\Repository as BoltRepository;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\Console\Input\InputInterface;
@@ -18,6 +21,11 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class AccountMigrate extends BaseCommand
 {
+    /** @var array */
+    private $success = [];
+    /** @var array */
+    private $errors = [];
+
     protected function configure()
     {
         $this->setName('account:migrate')
@@ -37,6 +45,11 @@ class AccountMigrate extends BaseCommand
         /** @var Repository\Account $accountRepo */
         $accountRepo = $app['storage']->getRepository(Entity\Account::class);
 
+        /** @var MarketPlaceRepository\Package $packageRepo */
+        $packageRepo = $app['storage']->getRepository(MarketPlaceEntity\Package::class);
+        /** @var MarketPlaceRepository\Stat $statRepo */
+        $statRepo = $app['storage']->getRepository(MarketPlaceEntity\Stat::class);
+
         $accounts = $accountRepo->findAll();
         if ($accounts === false) {
             return;
@@ -47,6 +60,10 @@ class AccountMigrate extends BaseCommand
             $output->writeln(sprintf('<info>Migrating %s (%s)</info>', $account->getEmail(), $account->getId()));
             $this->migrateAccount($membersAccountRepo, $membersOauthRepo, $account, $output);
         }
+
+        foreach ($this->errors as $error) {
+            $this->mergeAccountPackages($packageRepo, $statRepo, $error);
+        }
     }
 
     protected function migrateAccount(
@@ -55,9 +72,15 @@ class AccountMigrate extends BaseCommand
         Entity\Account $account,
         OutputInterface $output
     ) {
+        $app = $this->app;
+        /** @var Records $membersRecords */
+        $membersRecords = $app['members.records'];
+
+        $guid = $account->getId();
+        $email = strtolower($account->getEmail());
         $memberAccount = new MembersAccountEntity([
-            'guid'        => $account->getId(),
-            'email'       => $account->getEmail(),
+            'guid'        => $guid,
+            'email'       => $email,
             'displayname' => $account->getName(),
             'enabled'     => $account->isApproved(),
             'verified'    => true,
@@ -68,8 +91,8 @@ class AccountMigrate extends BaseCommand
 
         $membersOauth = new MembersOauthEntity([
             'id'                => Uuid::uuid4()->toString(),
-            'guid'              => $account->getId(),
-            'resource_owner_id' => $account->getId(),
+            'guid'              => $guid,
+            'resource_owner_id' => $guid,
             'password'          => $account->getPassword(),
             'enabled'           => $account->isApproved(),
         ]);
@@ -78,14 +101,59 @@ class AccountMigrate extends BaseCommand
             $this->insert($membersAccountRepo, $memberAccount);
             $this->insert($membersOauthRepo, $membersOauth);
 
-            $app = $this->app;
-            $app['members.records']->createProvision($account->getId(), 'local', $account->getId());
+            $membersRecords->createProvision($guid, 'local', $guid);
+
+            $this->success[$email] = $memberAccount;
         } catch (UniqueConstraintViolationException $e) {
-            $output->writeln(sprintf('<error>    Error: %s</error>', $e->getMessage()));
+            $this->errors[] = [
+                'guid'  => $guid,
+                'email' => $email,
+            ];
+
+            $output->writeln(sprintf('<error>Duplicate email: %s</error>', $account->getEmail()));
         }
     }
 
-    protected function insert(\Bolt\Storage\Repository $repo, $entity)
+    /**
+     * @param MarketPlaceRepository\Package $packageRepo
+     * @param MarketPlaceRepository\Stat    $statRepo
+     * @param array                         $error
+     */
+    protected function mergeAccountPackages(
+        MarketPlaceRepository\Package $packageRepo,
+        MarketPlaceRepository\Stat $statRepo,
+        array $error
+    ) {
+        $email = $error['email'];
+        /** @var MembersAccountEntity $account */
+        $account = $this->success[$email];
+        $oldGuid = $error['guid'];
+        $newGuid = $account->getGuid();
+
+        $packages = $packageRepo->findBy(['account_id' => $oldGuid]);
+        /** @var MarketPlaceEntity\Package $package */
+        foreach ($packages as $package) {
+            $package->setAccountId($newGuid);
+            $packageRepo->save($package);
+        }
+
+        $stats = $statRepo->findBy(['account_id' => $oldGuid]);
+        /** @var MarketPlaceEntity\Stat $stat */
+        foreach ($stats as $stat) {
+            $stat->setAccountId($newGuid);
+            $statRepo->save($stat);
+        }
+    }
+
+    /**
+     * @param BoltRepository $repo
+     * @param object         $entity
+     *
+     * @throws \Exception
+     *
+     * @return \Doctrine\DBAL\Driver\Statement|int|null
+     */
+    protected function insert(BoltRepository $repo, $entity)
     {
         $querySet = new QuerySet();
         $qb = $repo->em->createQueryBuilder();
@@ -98,7 +166,13 @@ class AccountMigrate extends BaseCommand
         return $result;
     }
 
-    protected function persist(\Bolt\Storage\Repository $repo, QuerySet $queries, $entity, $exclusions = [])
+    /**
+     * @param BoltRepository $repo
+     * @param QuerySet       $queries
+     * @param object         $entity
+     * @param array          $exclusions
+     */
+    protected function persist(BoltRepository $repo, QuerySet $queries, $entity, $exclusions = [])
     {
         $metadata = $repo->getClassMetadata();
 
