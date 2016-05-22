52bd1f17f7dd645f3f7b9f83ed8fa47b7b290567
Gawain Lynch <gawain.lynch@gmail.com>
2016-05-22 17:52:39 +0200

    Statistics service

--- extensions/local/bolt/site/src/Provider/MarketPlaceServiceProvider.php
+++ extensions/local/bolt/site/src/Provider/MarketPlaceServiceProvider.php
@@ -115,6 +115,12 @@ class MarketPlaceServiceProvider implements ServiceProviderInterface
                     'email'           => $app->share(function () use ($app) { return new Service\Email(); }),
                     'mail'            => $app->share(function () use ($app) { return new Service\MailService(); }),
                     'package_manager' => $app->share(function () use ($app) { return new Service\PackageManager($app['marketplace.composer.config']); }),
+                    'statistics'      => $app->share(function () use ($app) {
+                        /** @var Repository\Stat $repo */
+                        $repo = $app['storage']->getRepository(Entity\Stat::class);
+
+                        return new Service\Statistics($repo);
+                    }),
                 ]);
 
                 return $container;
