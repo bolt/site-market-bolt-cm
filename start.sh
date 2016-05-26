203716bbdd70f2584b3e03adacc4458b687c81ab
Gawain Lynch <gawain.lynch@gmail.com>
2016-05-26 20:03:11 +0200

    Remove more

--- start.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p /tmp/.composer
-cp config/github.json /tmp/.composer/auth.json
-./console migrations:migrate --no-interaction
-./composer config -g github-oauth.github.com `head config/github`
-./console orm:generate-proxies
-./console bolt:extension-tester
