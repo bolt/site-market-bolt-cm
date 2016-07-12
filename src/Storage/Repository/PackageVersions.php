ecb6d3e183f58f920aaec9f01713a2b068448773
Gawain Lynch <gawain.lynch@gmail.com>
2016-07-12 13:56:23 +0200

    Repository function to get lLatest compatible version

--- src/Storage/Repository/PackageVersions.php
+++ src/Storage/Repository/PackageVersions.php
@@ -4,6 +4,8 @@ namespace Bolt\Extension\Bolt\MarketPlace\Storage\Repository;
 
 use Bolt\Extension\Bolt\MarketPlace\Storage\Entity;
 use Bolt\Storage\Repository;
+use Composer\Semver\Constraint\Constraint;
+use Composer\Semver\VersionParser;
 
 /**
  * Package version repository.
@@ -17,7 +19,56 @@ class PackageVersions extends AbstractRepository
      */
     public function createQueryBuilder($alias = null)
     {
+        // Gotta fix this alias thing …
+        // HINT: The abstract overrides getLoadQuery() for a reason I can't quite remember
         return $this->em->createQueryBuilder()
-            ->from($this->getTableName(), 'package_versions');
+            ->from($this->getTableName(), $alias === 'packageversions' ? 'package_versions' :$alias);
+    }
+
+
+    /**
+     * @param string $packageId
+     * @param string $stability
+     * @param string $boltVersion
+     *
+     * @return array|bool
+     */
+    public function getLatestCompatibleVersion($packageId, $stability, $boltVersion)
+    {
+        $query = $this->getLatestVersionQuery($packageId, $stability);
+
+        $versionEntities = $this->findWith($query);
+        if ($versionEntities === false) {
+            return false;
+        }
+
+        $parser = new VersionParser();
+        $boltVersion = new Constraint('==', $parser->normalize($boltVersion));
+
+        /** @var Entity\PackageVersions $versionEntity */
+        foreach ($versionEntities as $versionEntity) {
+            $boltConstraints = $parser->parseConstraints(sprintf('%s,%s', $versionEntity->getBoltMin(), $versionEntity->getBoltMax()));
+            if ($boltConstraints->matches($boltVersion)) {
+                return $versionEntity;
+            }
+        }
+
+
+        return false;
+    }
+
+    public function getLatestVersionQuery($packageId, $stability)
+    {
+        $qb = $this->createQueryBuilder('v');
+        $qb
+            ->select('*')
+            ->where('v.package_id = :package_id')
+            ->andWhere('v.stability = :stability')
+            ->orderBy('v.version', 'DESC')
+            ->setParameter('package_id', $packageId)
+            ->setParameter('stability', $stability)
+        ;
+
+        return $qb;
     }
 }
